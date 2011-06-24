62f33d217fdfcf0ec83bfca84952a9ad58e9b83b
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2011-06-24 20:34:43 +0200

    Updated Readme.

--- Samples/Chap21/todo/EdrTest/build.bat
+++
@@ -1,2 +0,0 @@
-dmd -H -ofEdrLib.dll -L/IMPLIB -I..\..\..\ ..\..\..\win32.lib -I. -version=Unicode -version=WIN32_WINNT_ONLY -version=WindowsNTonly -version=Windows2000 -version=Windows2003 -version=WindowsXP -version=WindowsVista dllmodule.d EdrLib.d
-dmd -ofEdrTest.exe -I..\..\..\ ..\..\..\win32.lib -I. -version=Unicode -version=WIN32_WINNT_ONLY -version=WindowsNTonly -version=Windows2000 -version=Windows2003 -version=WindowsXP -version=WindowsVista EdrTest.d EdrLib.lib
--- Samples/Chap21/todo/StrProg/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-dmd -H -ofStrLib.dll -L/IMPLIB -I..\..\..\ ..\..\..\win32.lib -I. -version=Unicode -version=WIN32_WINNT_ONLY -version=WindowsNTonly -version=Windows2000 -version=Windows2003 -version=WindowsXP -version=WindowsVista StrLib.d dllmodule.d
-
-dmd -ofStrProg.exe -I..\..\..\ ..\..\..\win32.lib -I. -version=Unicode -version=WIN32_WINNT_ONLY -version=WindowsNTonly -version=Windows2000 -version=Windows2003 -version=WindowsXP -version=WindowsVista StrProg.d StrLib.lib StrProg.res
